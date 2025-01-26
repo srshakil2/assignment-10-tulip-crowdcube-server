d12eca5822b619364fadca2d2fc4912018b6c618
srshakil2 <rh3642921@gmail.com>
2025-01-27 02:26:25 +0600

    all done

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const port = process.env.PORT || 5500;
 app.use(cors());
 app.use(express.json());
 
-const verifyUser = (req, res, next) => {
-  const userEmail = req.query.userEmail;
-  if (!userEmail) {
-    return res.status(401).send({ error: "Unauthorized access" });
-  }
-  next();
-};
+// const verifyUser = (req, res, next) => {
+//   const userEmail = req.query.userEmail;
+//   if (!userEmail) {
+//     return res.status(401).send({ error: "Unauthorized access" });
+//   }
+//   next();
+// };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mp1yd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const campaignCollection = client
       .db("crowdcube")
@@ -47,6 +47,7 @@ async function run() {
 
     app.get("/campaign/:id", async (req, res) => {
       const id = req.params.id;
+      // console.log(id, "------------");
       const query = { _id: new ObjectId(id) };
       const result = await campaignCollection.findOne(query);
       res.send(result);
@@ -59,9 +60,10 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/campaign", verifyUser, async (req, res) => {
-      const userEmail = req.query.userEmail;
-      const query = { userEmail };
+    app.get("/campaign/useradd/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email, "----------");
+      const query = { userEmail: email };
       const campaigns = await campaignCollection.find(query).toArray();
       res.send(campaigns);
     });
@@ -78,11 +80,32 @@ async function run() {
         res.send({ success: false, message: "Failed to delete campaign." });
       }
     });
+    // update data
+    app.patch("/campaign/updateone/:id", async (req, res) => {
+      const id = req.params?.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          title: data?.title,
+          type: data?.type,
+          description: data?.description,
+          minDonation: data?.minDonation,
+          deadline: data?.date,
+          userEmail: data?.email,
+          userName: data?.name,
+          image: data?.image,
+        },
+      };
+      const result = await campaignCollection.updateOne(query, updatedDoc);
+      console.log(result);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
